3c90cb784a538feb5302b6a0b09535c6c15bc820
Brett Thieman <bthieman3@huskers.unl.edu>
2021-08-26 21:54:40 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/*
+Author: Brett Thieman
+Date: 2021/08/26
+
+A simple program stating my name and major
+*/
+
+  #include<stdlib.h>
+  #include<stdio.h>
+  
+  int main(int argc, char **argv) {
+      
+      printf("Brett Thieman;\n");
+      printf("Computer Science\n");
+      
+      return 0;
+      
+  }
